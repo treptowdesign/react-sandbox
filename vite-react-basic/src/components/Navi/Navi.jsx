e79d7ae6bceea316a74913808cc8d6c01ea3c0e9
Kyle Treptow <kyletreptowdesign@gmail.com>
2024-12-03 20:23:30 -0800

    abso import at src and navi component

--- vite-react-basic/src/components/Navi/Navi.jsx
+++ vite-react-basic/src/components/Navi/Navi.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 // import { useState } from 'react'
+import './Navi.sass'
 
 function Navi() {
+
     return (
-      <ul>
+      <ul className="navi">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/tic-tac-toe">Tic Tac Toe</Link></li>
         <li><Link to="/counter">Counter</Link></li>
